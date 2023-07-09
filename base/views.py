f25df9f2f24d43e0a45943c9a08ee498ed7bcd4e
Ayush <ayush.kj@iitg.ac.in>
2023-07-09 18:41:03 +0530

    payementgateway integrated

--- base/views.py
+++ base/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+import RazorPay
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+
+def home(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        amount = 5000
+
+        client = RazorPay.Client(auth=("rzp_test_Po0ByeOrpzroyx", "5LQbaXtQSjNp7dtHHdltpqkN"))
+
+        payment = client.order.create({'amount': amount, 'currency': 'INR',
+                                       'payment_capture': '1'})
+    return render(request, 'index.html')
+
+@csrf_exempt
+def success(request):
+    return render(request, "success.html")
